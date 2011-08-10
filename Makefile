4944896fe71106c65b3407bd5b8aa2b69945a7a0
Haruka Kataoka <hrk8@drumsoft.com>
2011-08-10 21:20:06 +0900

    wrote README and fine Makefile

--- Makefile
+++ Makefile
@@ -1,17 +1,19 @@
 MAKE = make
 CP = cp
 
+all: otoperld otoperld-start.pl
+
 otoperld: otoperld-src/otoperld
 	$(CP) $^ .
 
 otoperld-start.pl: otoperld-src/otoperld-start.pl
 	$(CP) $^ .
 
-otoperld-src/otoperld:
+otoperld-src/otoperld: otoperld-src/*.h otoperld-src/*.c
 	$(MAKE) -C otoperld-src
 
 clean: 
 	$(MAKE) clean -C otoperld-src
 
-test: otoperld otoperld-start.pl
+run: all
 	./otoperld
--- otoperld-start.pl
+++ otoperld-start.pl
@@ -1,3 +1,22 @@
+# OtoPerl - live sound programming environment with Perl.
+# otoperld-start.pl - startpoint for OtoPerl.
+
+#   OtoPerl - live sound programming environment with Perl.
+#   Copyright (C) 2011- Haruka Kataoka
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
 

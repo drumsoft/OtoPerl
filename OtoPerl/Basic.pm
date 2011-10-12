c24f89a080c0b635223385d46bd6ceb9e4a589b0
Haruka Kataoka <hrk8@drumsoft.com>
2011-10-13 08:22:34 +0900

    add Filter to Basic.pm, and sample modified

--- OtoPerl/Basic.pm
+++ OtoPerl/Basic.pm
@@ -4,6 +4,10 @@ my $PI2 = 3.14159265 * 2;
 my $sample_rate = 48000;
 my $freq_tuning = 440;
 
+sub default_wave {
+	sin(shift);
+}
+
 my $osc_sin_fact = $PI2 / $sample_rate;
 sub osc_sin { # ($frame, $freq)
 	sin( $osc_sin_fact * (shift) * (shift) );
@@ -24,6 +28,108 @@ sub scalednote {
 	my $s = shift;
 	my $n = shift;
 	$s->[$n % @$s] + 12 * int($n / @$s);
+}
+
+
+package OtoPerl::Basic::OSC;
+
+sub new {
+	my ($class, $freq, $function, $frame) = @_;
+	my $self = bless {
+		frame => $frame,
+		phase => 0,
+		function => $function || \&OtoPerl::Basic::default_wave
+	}, $class;
+	$self->freq($freq);
+	return $self;
+}
+
+sub freq {
+	my ($self, $freq) = @_;
+	if (defined $freq) {
+		$self->{freq} = $freq;
+		$self->{dphase} = $freq * $PI2 / $sample_rate;
+		$self;
+	}else{
+		$self->{freq};
+	}
+}
+
+sub val {
+	my ($self, $frame) = @_;
+	$self->{phase} += $self->{dphase} * ($frame - $self->{frame});
+	$self->{frame} = $frame;
+	$self->{function}->( $self->{phase} );
+}
+
+sub next {
+	my ($self) = @_;
+	$self->{phase} += $self->{dphase};
+	$self->{frame}++;
+	$self->{function}->( $self->{phase} );
+}
+
+
+package OtoPerl::Basic::EG;
+
+sub new {
+	my ($class, $freq, $function) = @_;
+	my $self = {
+		freq => $freq,
+		function => $function || \&OtoPerl::Basic::osc_sin
+	};
+	return bless $self, $class;
+}
+
+sub val {
+	my ($self, $frame) = @_;
+	$self->{function}->($frame, $self->{freq});
+}
+
+
+package OtoPerl::Basic::Filter;
+
+sub new {
+	my ($class) = @_;
+	my $self = {
+		x => 0,
+		x1 => 0,
+		x2 => 0,
+		y => 0,
+		y1 => 0,
+		y2 => 0
+	};
+	return bless $self, $class;
+}
+
+sub set {
+	my ($v, $freq, $q) = @_;
+
+	my $w0 = $PI2 * $freq / $sample_rate;
+	my $alpha = sin($w0) / (2 * $q);
+	my $cs = cos($w0);
+
+	my $b1 =   1 - $cs;
+	my $b0 = $b1 / 2;
+	my $b2 = $b0;
+	my $a0 =   1 + $alpha;
+	my $a1 =  -2 * $cs;
+	my $a2 =   1 - $alpha;
+
+	$v->{k1} = $b0/$a0;
+	$v->{k2} = $b1/$a0;
+	$v->{k3} = $b2/$a0;
+	$v->{k4} = $a1/$a0;
+	$v->{k5} = $a2/$a0;
+}
+
+sub val {
+	my $v = shift;
+	$v->{x2} = $v->{x1};	$v->{x1} = $v->{x};	$v->{x} = shift;
+	$v->{y2} = $v->{y1};	$v->{y1} = $v->{y};
+
+	$v->{y} = $v->{k1}*$v->{x} + $v->{k2}*$v->{x1} + $v->{k3}*$v->{x2}
+                               - $v->{k4}*$v->{y1} - $v->{k5}*$v->{y2};
 }
 
 1;
--- otoperl-sample.pl
+++ otoperl-sample.pl
@@ -8,22 +8,44 @@ my $f5 = 500;
 my $freqr = 1000;
 my $f6 = 1000;
 
+my $PI2 = 3.14159265 * 2;
+my $sample_rate = 48000;
+my $freq_tuning = 440;
+
+my $osc_sin_fact = $PI2 / $sample_rate;
+
+my $osc1_fact    = OtoPerl::Basic::freq_bynote(63) * $PI2 / $sample_rate;
+my $osc1_fact_am = 1.3                             * $PI2 / $sample_rate;
+
+my $osc2_fact    = OtoPerl::Basic::freq_bynote(24)        / $sample_rate;
+my $osc2_fact_am = 0.3                             * $PI2 / $sample_rate;
+
 sub perl_render {
 	my $size = shift;
 	my $channels = shift;
 	my (@w, $i, $v, @v, $k, $f, $amp);
 	my $m = 4;
-	my $f1 = OtoPerl::Basic::freq_bynote(70);
-	my $f2 = OtoPerl::Basic::freq_bynote(63);
 	my $f3 = OtoPerl::Basic::freq_bynote(34);
 
 	for ($i = $size-1; $i >= 0; $i--) {
 
 		# sine > (sine LFO > AM)
-		my $v1 = 0.8 * OtoPerl::Basic::osc_sin( $frame, 1.3 ) * OtoPerl::Basic::osc_sin( $frame, $f2 );
+		my $v1 = 0.8 * sin( $osc1_fact_am * $frame ) * 
+			sin( $osc1_fact * $frame );
 
 		# square > (sine LFO > AM)
-		my $v2 = 0.5 * OtoPerl::Basic::osc_sin( $frame, 0.3 ) * OtoPerl::Basic::osc_square( $frame, $f3 );
+		my $v2 = $osc2_fact * $frame;
+		$v2 = 0.5 * sin( $osc2_fact_am * $frame ) * 
+			( $v2 - int($v2) > 0.5 ? 1 : -1 );
+
+		# random note > sine > (saw envelope > AM)
+		if ($nn < $frame) {
+			$nnl = 6000 * ( int(rand(2)) + 1 );
+			$nn = $frame + $nnl;
+		}
+		$amp  = ($nn - $frame) / $nnl;
+		$amp  = $amp > 0 ? ($amp > 1 ? 1 : $amp) : 0;
+		my $v3 = $amp * (rand(2) - 1);
 
 		# random note > sine > (saw envelope > AM)
 		if ($trigger < $frame) {
@@ -34,15 +56,6 @@ sub perl_render {
 		$amp  = ($trigger - $frame) / $triggerl;
 		$amp  = $amp > 0 ? ($amp > 1 ? 1 : $amp) : 0;
 		my $v4 = OtoPerl::Basic::osc_sin( $frame, $freqr ) > 0 ? $amp : -$amp;
-
-		# random note > sine > (saw envelope > AM)
-		if ($nn < $frame) {
-			$nnl = 6000 * ( int(rand(2)) + 1 );
-			$nn = $frame + $nnl;
-		}
-		$amp  = ($nn - $frame) / $nnl;
-		$amp  = $amp > 0 ? ($amp > 1 ? 1 : $amp) : 0;
-		my $v3 = $amp * (rand(2) - 1);
 
 		# downstair note > sine
 		if (0 == $frame % 12000) {
